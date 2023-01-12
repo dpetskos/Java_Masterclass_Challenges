b395483167bd95043d2446bc942a88245c3504a2
dpetskos <dpetskos@gmail.com>
2023-01-12 19:29:33 +0200

    Init

---
+++ src/FloatingPointPrecisionChallenge.java
@@ -0,0 +1,11 @@
+public class FloatingPointPrecisionChallenge {
+    public static void main(String[] args) {
+        double pounds = 200;
+        double kilograms = pounds * 0.45359237;
+        System.out.println(kilograms);
+
+
+    }
+}
+
+
